52c52c945b81b8b9967e3a79928de8b06ebe600d
nkawaller <63615939+nkawaller@users.noreply.github.com>
2020-11-10 09:47:39 -0500

    added a new component

--- src/App.vue
+++ src/App.vue
@@ -1,20 +1,12 @@
 <template>
-    <ul>
-        <learning-resource 
-            v-for="resource in storedResources" 
-            :key='resource.id' 
-            :title="resource.title" 
-            :description="resource.description"
-            :link="resource.link"
-            ></learning-resource>
-    </ul>
+    <stored-resources :resources="storedResources"></stored-resources>
 </template>
 
 <script>
-import LearningResource from './components/learning_resources/LearningResource.vue';
+import StoredResources from './components/learning_resources/StoredResources.vue';
 export default {
     components: {
-        LearningResource
+        StoredResources
     },
     data() {
         return{
